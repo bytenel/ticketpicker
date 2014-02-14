fde271ac48d59b19f55525676361dfe2abfc8ced
Ben Nelson <nelson.ben.c@gmail.com>
2014-02-14 15:39:05 -0700

    adding in libraries, quit button
    adding in the bootstrap and jquery libraries, adding in a button that
    has quit functionality on click

--- scripts/viewmodel/baseUI.js
+++ scripts/viewmodel/baseUI.js
@@ -1,6 +1,13 @@
 /**
  * Created by ben.nelson on 2/14/14.
+ * reference api:
+ * https://github.com/rogerwang/node-webkit/wiki/Native-UI-API-Manual
  */
 var gui = require('nw.gui');
 
 var menu = new gui.Menu();
+
+function quit()
+{
+  gui.App.quit();
+}
