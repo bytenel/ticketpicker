6bcc0b9e381923fa44e11bfa5de3a61445f0d23b
Ben Nelson <nelson.ben.c@gmail.com>
2014-02-28 14:56:15 -0700

    karma infrastructure
    getting karma set up and a base test to ensure the test framework works
    installed

--- karma.conf.js
+++ karma.conf.js
@@ -1,48 +1,70 @@
 // Karma configuration
-// http://karma-runner.github.io/0.10/config/configuration-file.html
+// Generated on Fri Feb 28 2014 14:30:59 GMT-0700 (Mountain Standard Time)
 
 module.exports = function(config) {
   config.set({
+
     // base path, that will be used to resolve files and exclude
     basePath: '',
 
-    // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['jasmine'],
+
+    // frameworks to use
+    frameworks: ['qunit'],
+
 
     // list of files / patterns to load in the browser
     files: [
+      'app/bower_components/jquery/jquery.js',
       'app/bower_components/angular/angular.js',
-      'app/bower_components/angular/angular-mocks.js',
-      'app/scripts/*.js',
-      'app/scripts/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
+      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-resource/angular-resource.js',
+      'scripts/**/*.js',
+      'spec/**/*_tests.js'
     ],
 
-    // list of files / patterns to exclude
-    exclude: [],
+
+    // list of files to exclude
+    exclude: [
+      'karma.config.js',
+      'scripts/viewmodel/baseUI.js'
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
+
 
     // web server port
-    port: 8080,
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
 
     // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // Start these browsers, currently available:
     // - Chrome
     // - ChromeCanary
     // - Firefox
-    // - Opera
-    // - Safari (only Mac)
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
     // - PhantomJS
-    // - IE (only Windows)
-    browsers: ['Chrome'],
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['PhantomJS'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
 
     // Continuous Integration mode
---
+++ spec/controllers/main_tests.js
@@ -0,0 +1,7 @@
+/**
+ * Created by ben.nelson on 2/28/14.
+ */
+test("base test", function(){
+    ok(1 == "1", "Test framework works!");
+});
+
