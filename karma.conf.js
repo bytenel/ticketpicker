a25f4fbf3b1a36b283059383eb294eb95ef9c713
Ben Nelson <nelson.ben.c@gmail.com>
2014-04-02 15:31:22 -0600

    fixes #3
    adding in ng-sanitize so angular knows to compile my html strings

--- karma.conf.js
+++ karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function(config) {
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/bower_components/angular-resource/angular-resource.js',
+      'app/bower_components/angular-sanitize/angular-sanitize.js',
       'scripts/app.js',
       'scripts/services/**/*.js',
       'scripts/**/*.js',
