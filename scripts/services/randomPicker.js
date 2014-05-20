787137a0e7f381024ade95caed9b55cdc0d7ee48
Ben Nelson <nelson.ben.c@gmail.com>
2014-05-20 16:00:41 -0600

    finishing up the random picker service

--- scripts/services/randomPicker.js
+++ scripts/services/randomPicker.js
@@ -1,6 +1,33 @@
 //random user picking algorithm based on spec
+
 ticketpicker.service("$randomPicker", function(){
-		this.select = function(list){
+		this.select = function(list, nonwinning){
+			if(list.length == 0)
+				return;
 			
+			if(list.length == 1)
+				return list[0];
+
+			var nonwinners = [];
+			if(!nonwinning)
+			{
+				nonwinners = list.filter(function(item){
+					return item.lastWin == ' ' || item.lastWin == null || item.lastWin == '';
+				});
+			}
+			else{
+				return list[Math.floor((Math.random() * list.length) + 1)];
+			}
+
+			if(nonwinners.length > 0)
+				return this.select(nonwinners, true);
+
+			var smallest_winner = { lastWin: moment() };
+			list.forEach(function(item){
+				if(moment(new Date(item.lastWin)).isBefore(smallest_winner.lastWin))
+					smallest_winner = item;
+			});
+
+			return smallest_winner;
 		}
 });
--- spec/services/randomPicker_tests.js
+++ spec/services/randomPicker_tests.js
@@ -20,3 +20,41 @@ var rando_init = {
         equal(object, undefined, "When selecting from an empty array, result should be undefined.");
         notStrictEqual(object, null, "When selecting from an empty array, result should not be null.");
     });
+
+    test("returns the only user present in a one person array", function(){
+        // arrange
+        var user = {name: 'ben', lastWin: ''};
+        var users = [user];
+
+        // act
+        var object = this.$service.select(users);
+
+        // assert
+        deepEqual(object, user, "The user selected should be the only one present.");
+    });
+
+    test("out of two users, picks the one without a win date", function(){
+        // arrange
+        var user2 = {name: 'ben', lastWin: ''};
+        var user1 = {name: 'ben1', lastWin: '1/2/13'};
+        var users = [user1,user2];
+
+        // act
+        var object = this.$service.select(users);
+
+        // assert
+        deepEqual(object, user2, "The user selected should be the one without a win.");
+    });
+
+    test("out of two users, picks the one with the eariler win date", function(){
+        // arrange
+        var user2 = {name: 'ben', lastWin: '1/1/13'};
+        var user1 = {name: 'ben1', lastWin: '1/2/13'};
+        var users = [user1,user2];
+
+        // act
+        var object = this.$service.select(users);
+
+        // assert
+        deepEqual(object, user2, "The user selected should be the one with the earlier win.");
+    });
